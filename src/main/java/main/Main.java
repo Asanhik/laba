f411c1d554a74b452bc691c526cb7462e62eb4cb
Your Name <you@example.com>
2019-02-02 16:16:35 +0600

    init

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader; 
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Привет! Это лабораторная работа.");
+    }
+
+    public static int getResult(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/main/TestMain.java
@@ -0,0 +1,13 @@
+package main; 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+public class TestMain {
+    @Test
+    public void testResult() {
+	Main main = new Main();  
+	double result_number = main.getResult(12, 4);
+	double number = 48; 
+	assertEquals(number, result_number); 
+    }    
+}
